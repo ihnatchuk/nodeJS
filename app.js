121333d49968b7869288f11e61f90d4b34f68610
ihnatchuk <ihnatchukpp@gmail.com>
2023-03-04 00:36:40 +0200

    init1

--- app.js
+++ app.js
@@ -1,111 +1,259 @@
-const path = require('node:path')
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
+const path = require('path')
 
+const express = require('express')
+const app = express()
+app.listen(5000)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
-// завдання від Віктора з сортуванням
-// const sortFiles=(dirPath, targetDirPath, gender)=>{
-//     fs.readdir(path.join(__dirname,'students', dirPath),{withFileTypes:true},(err,files)=>{
-//             if (err) throw new Error(err.message)
-//             console.log(files);
+// const users = [
+//     {
+//         name: 'Oleh',
+//         age: 19,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Anton',
+//         age: 22,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Anya',
+//         age: 25,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Ielizavetta',
+//         age: 35,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Cocos',
+//         age: 70,
+//         gender: 'mixed'
+//     }
+// ]
 //
-//             for (const file of files) {
-//                 if (file.isFile()){
-//                     console.log(file.name);
-//
-//                     fs.readFile(path.join(__dirname,'students', dirPath, file.name),(err,data)=>{
-//                         if (err) throw new Error(err.massage)
-//                         console.log(JSON.parse(data));
-//                         if (JSON.parse(data).gender===gender){
-//                             fs.rename(
-//                                 path.join(__dirname,'students',dirPath,file.name),
-//                                 path.join(__dirname,'students',targetDirPath,file.name),
-//                                 (err)=> {
-//                                     if (err) throw new Error(err.message)
-//                                 }
-//                             )
-//                         }
-//                     })
-//                 }
-//             }
-//         }
-//     )
-// }
-//
-// sortFiles('boys','girls','female')
-// sortFiles('girls','boys','male')
+// fs.writeFile(path.join(__dirname,'users.json'),JSON.stringify(users),(err)=>{
+//     if (err) throw new Error(err.message)
+// })
 
-// ДЗ:
-// Створіть папку
-// В тій папці створіть 5 папок і 5 файлів
-// І за допомогою модулю fs виведіть в консоль, чи це папка чи це файл
-// !!все робіть кодом, не руками
-//
-// FILE: {fileName}
-// FOLDER: {folderName}
-
-const createFiles = async (dirName,n) => {
-    const arr=[]
-    for (let i = 0; i < n; i++) {
-        arr.push(i);
+const read = async () => {
+    const data = fs.readFile(path.join(__dirname, 'users.json'), {encoding: 'utf8'})
+    return await data
+}
+app.get('/users', (req, res) => {
+    try {
+        read().then(data => res.send(data))
+    } catch (e) {
+        console.log(e.message);
     }
-    fs.mkdir(path.join(__dirname, dirName),{recursive:true}, (err) => {
-        if (err) throw new Error(err.message)
-    })
-    const filesPromises = arr.map(async (i)=>{
-        await fs.mkdir(path.join(__dirname, dirName, `${i}`),{recursive:true}, (err) => {
-            if (err) throw new Error(err.message)
-        })
-        await fs.writeFile(path.join(__dirname, dirName, `${i}.txt`), `student id${i}`, (err) => {
-            if (err) throw new Error(err.message)
-        })
-    })
 
-    const promises=Promise.allSettled(filesPromises)
-    console.log(promises);
-}
-// const readFiles=async (dirName)=>{
-//     await fs.readdir(path.join(__dirname, dirName), {withFileTypes: true}, (err, files) => {
-//         if (err) throw new Error(err.message)
-//         for (let file of files) {
-//             if (file.isFile()) {
-//                 console.log(`FILE: ${file.name}`)
-//             }
-//             if (file.isDirectory()) {
-//                 console.log(`FOLDER: ${file.name}`)
-//             }
-//         }
-//     })
-// }
-const readFiles=async (dirName)=>{
-    await fs.readdir(path.join(__dirname, dirName), (err, files) => {
-        if (err) throw new Error(err.message)
-        for (let file of files) {
-            fs.stat(path.join(__dirname,dirName,file),(err, stats)=>{
-                const isFile=stats.isFile()
-            if (isFile) {
-                console.log(`FILE: ${file}`)
+})
+app.get('/users/:userId', (req, res) => {
+    const {userId} = req.params
+    try {
+        read().then(data => {
+            const users = JSON.parse(data)
+            if ((+userId >= 0) && (+userId < users.length)) {
+                res.send(users[userId])
+            } else {
+                res.send(`incorrect userId. 0<userId<${users.length}`)
             }
-            if (!isFile) {
-                console.log(`FOLDER: ${file}`)
-            }
-            })
+        })
+    } catch (e) {
+        console.log(e.message);
+    }
+})
+
+// app.post('/users',(req, res)=>{
+//     console.log(req.json(body));
+//
+//     // fs.readFile(path.join(__dirname,'users.json'), {encoding:'utf8'},(err, data)=>{
+//     //     const users=JSON.parse(data)
+//     //     if ((+userId>0) && (+userId<users.length)){
+//     //         res.send(JSON.parse(data)[userId])
+//     //     }else{
+//     //         res.send(`uncorrect userId. 0<userId<${users.length}`)
+//     //     }
+//     // })
+// })
+
+app.post('/users', (req, res) => {
+    const body = req.body;
+    let reqMassage = ''
+    let genderIsValid = false;
+
+    if ((typeof body.age !== 'undefined') && (!isNaN(+body.age))) {
+        if (+body.age > 0) {
+            ageIsValid = true
+        } else {
+            ageIsValid = false
+            reqMassage = 'Age is incorrect'
         }
-    })
-}
+    } else {
+        ageIsValid = false
+        reqMassage = 'Data is incorrect'
+    }
 
-const createRead=async (dirName,n)=>{
-    await createFiles(dirName,n).then(readFiles(dirName))
-}
+    if (typeof body.name !== 'undefined') {
+        if ((typeof body.name === 'string') && (body.name.length > 1)) {
+            nameIsValid = true
+        } else {
+            nameIsValid = false
+            reqMassage = 'Name is incorrect'
+        }
+    } else {
+        nameIsValid = false
+        reqMassage = 'Data is incorrect'
+    }
 
-createRead('hw1',10)
+    if (typeof body.gender !== 'undefined') {
+        if (body.gender === 'male' || body.gender === 'female' || body.gender === 'mixed') {
+            genderIsValid = true
+        } else {
+            genderIsValid = false
+            reqMassage = 'Gender is incorrect'
+        }
+    } else {
+        nameIsValid = false
+        reqMassage = 'Data is incorrect'
+    }
+    console.log("body", body);
+    // users.push(body);
+    if (ageIsValid && nameIsValid && genderIsValid) {
+        read().then(data=>{
+            const users=[...JSON.parse(data)]
+            users.push(body)
+            fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users), (err) => {
+                if (err) throw new Error(err.message)
+            })
+
+        })
+
+        res.status(201).json({
+            message: 'User created!'
+        })
+    } else {
+        res.status(400).json({
+            message: reqMassage
+        })
+    }
+
+})
+
+app.delete('/users/:userId', (req, res) => {
+    const {userId}=req.params
+    try{
+        read().then(data=> {
+            let users=[...JSON.parse(data)]
+
+            if ((+userId+1>0)&&(+userId<users.length)){
+
+                users.splice(userId,1)
+
+                fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users), (err) => {
+                    if (err) throw new Error(err.message)
+                })
+                res.send(`User ${userId} deleted!`)
+
+            }else {
+                res.send('UserId not found')
+            }
+
+        })
+
+    }catch (e) {
+        console.log(e.message)
+    }
+})
+
+app.put('/users/:userId', (req, res) => {
+    const body = req.body;
+    const {userId}=req.params
+
+    let reqMassage = ''
+    let ageIsValid = false;
+    let nameIsValid = false;
+    let genderIsValid = false;
 
 
+    console.log(body);
+    console.log(userId);
+
+    try{
+        read().then(data=> {
 
 
+            let users=[...JSON.parse(data)]
 
 
+            console.log(body);
+            console.log(userId);
 
+            if ((+userId+1>0)&&(+userId<users.length)){
 
+                if ((typeof body.age !== 'undefined') && (!isNaN(+body.age))) {
+                    if (+body.age > 0) {
+                        ageIsValid = true
+                    } else {
+                        ageIsValid = false
+                        reqMassage = 'Age is incorrect'
+                    }
+                } else {
+                    ageIsValid = false
+                    reqMassage = 'Data is incorrect'
+                }
 
+                if (typeof body.name !== 'undefined') {
+                    if ((typeof body.name === 'string') && (body.name.length > 1)) {
+                        nameIsValid = true
+                    } else {
+                        nameIsValid = false
+                        reqMassage = 'Name is incorrect'
+                    }
+                } else {
+                    nameIsValid = false
+                    reqMassage = 'Data is incorrect'
+                }
 
+                if (typeof body.gender !== 'undefined') {
+                    if (body.gender === 'male' || body.gender === 'female' || body.gender === 'mixed') {
+                        genderIsValid = true
+                    } else {
+                        genderIsValid = false
+                        reqMassage = 'Gender is incorrect'
+                    }
+                } else {
+                    nameIsValid = false
+                    reqMassage = 'Data is incorrect'
+                }
+                console.log("body", body);
+                // users.push(body);
+                if (ageIsValid && nameIsValid && genderIsValid) {
+                    const users=[...JSON.parse(data)]
+                    users[userId]={...body}
+                    fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users), (err) => {
+                        if (err) throw new Error(err.message)
+                    })
 
+                    res.status(201).json({
+                        message: `User ${userId} updated!`
+                    })
+                } else {
+                    res.status(400).json({
+                        message: reqMassage
+                    })
+                }
+
+            }else {
+                res.send('UserId not found')
+            }
+
+        })
+
+    }catch (e) {
+        console.log(e.message)
+    }
+})
--- app3.js
+++
@@ -1,8 +0,0 @@
-const fs=require('node:fs')
-const path = require("node:path");
-
-fs.stat(path.join(process.cwd(), 'app.js'), (err,stats)=>{
-    console.log(stats)
-    console.log(stats.isFile());
-});
-
